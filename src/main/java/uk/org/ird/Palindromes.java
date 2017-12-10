55f51f1cbd9c55f1bd58627a1edd3d14216f7748
ird <31051194+ird@users.noreply.github.com>
2017-12-10 15:16:52 +0000

    initial commit

---
+++ src/main/java/uk/org/ird/Palindromes.java
@@ -0,0 +1,9 @@
+package uk.org.ird;
+
+public class Palindromes {
+    private static int last = 1;
+    public static int next() {
+        // TODO: Palindrome generator function
+        return last;
+    }
+}
